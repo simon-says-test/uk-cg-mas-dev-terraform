2217f79e6ae84585331b72d4dd20cd8b3e054c70
Simon Thomas <simon.thomas@civica.co.uk>
2020-12-19 13:05:41 +0000

    add nsg

--- main.tf
+++ main.tf
@@ -37,18 +37,33 @@ resource "azurerm_resource_group" "vnet" {
   location = "uksouth"
 }
 
+resource "azurerm_network_security_group" "primary" {
+  name                = "DEV-AZ-NSG"
+  location            = azurerm_resource_group.vnet.location
+  resource_group_name = azurerm_resource_group.vnet.name
+  security_rule {
+    priority = 110
+    name = "RDP-VMtest"
+    source_port_range = "*"
+    destination_port_range = "3389"
+    protocol = "any"
+    source_address_prefixes = "212.28.31.6,213.143.143.69,213.143.143.68,213.143.146.159"
+    access = "Allow"
+    direction = "Inbound"
+  }
+}
+
 resource "azurerm_virtual_network" "primary" {
   name                = "DEV-AZ-VNET"
   address_space       = ["192.168.16.0/23"]
   location            = azurerm_resource_group.vnet.location
   resource_group_name = azurerm_resource_group.vnet.name
-}
 
-resource "azurerm_subnet" "primary" {
-  name                 = "DEV-AZ-Sub01"
-  resource_group_name  = azurerm_resource_group.vnet.name
-  virtual_network_name = azurerm_virtual_network.primary.name
-  address_prefixes     = ["192.168.16.0/24"]
+  subnet {
+    name           = "DEV-AZ-Sub01"
+    address_prefix = "192.168.16.0/24"
+    security_group = azurerm_network_security_group.primary.id
+  }
 }
 
 resource "azurerm_network_interface" "example" {
