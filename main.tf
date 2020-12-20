6d122f3ca2d6f79a35aa95cd95308c9c4b18d658
Simon Thomas <simon.thomas@civica.co.uk>
2020-12-20 06:51:44 +0000

    add storage and tidy

--- main.tf
+++ main.tf
@@ -22,41 +22,6 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "vnet" {
-  name     = "Admin-VNet"
-  location = "uksouth"
-}
-
-resource "azurerm_network_security_group" "primary" {
-  name                = "DEV-AZ-NSG"
-  location            = azurerm_resource_group.vnet.location
-  resource_group_name = azurerm_resource_group.vnet.name
-  security_rule {
-    priority = 110
-    name = "RDP-VMtest"
-    source_port_range = "*"
-    destination_address_prefix = "*"
-    destination_port_range = "3389"
-    protocol = "*"
-    source_address_prefixes = ["212.28.31.6","213.143.143.69","213.143.143.68","213.143.146.159"]
-    access = "Allow"
-    direction = "Inbound"
-  }
-}
-
-resource "azurerm_virtual_network" "primary" {
-  name                = "DEV-AZ-VNET"
-  address_space       = ["192.168.16.0/23"]
-  location            = azurerm_resource_group.vnet.location
-  resource_group_name = azurerm_resource_group.vnet.name
-
-  subnet {
-    name                = "DEV-AZ-Sub01"
-    address_prefix      = "192.168.16.0/24"
-    security_group      = azurerm_network_security_group.primary.id
-  }
-}
-
 module "azure_windows_vm_1" {
   count                   = length(local.vm_params)
   source                  = "./azure_windows_vm"
---
+++ storage.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "storage" {
+  name     = "Admin-Storage"
+  location = "uksouth"
+}
+
+resource "azurerm_storage_account" "primary" {
+  name                     = "stadmindata001"
+  resource_group_name      = azurerm_resource_group.storage.name
+  location                 = azurerm_resource_group.storage.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "primary" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.primary.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "primary" {
+  name                   = "matrix-icon.ico"
+  storage_account_name   = azurerm_storage_account.primary.name
+  storage_container_name = azurerm_storage_container.primary.name
+  type                   = "Block"
+  source                 = "matrix_code.ico"
+}
