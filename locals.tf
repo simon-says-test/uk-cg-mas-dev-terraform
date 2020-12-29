ef90c7919d8c9dc1257ea906611f5bd14f260db0
Simon Thomas <simon.thomas@civica.co.uk>
2020-12-29 16:17:09 +0000

    trigger whole new build

--- locals.tf
+++ locals.tf
@@ -1,12 +1,12 @@
 locals {
     vm_params = [ 
         { 
-          computer_name = "simon"
+          computer_name = "simon-dev"
           name = "vm-ukcg-dev-simon"  
           resource_group_name = "rg-ukcg-dev-simon"
         },
         { 
-          computer_name = "warren"
+          computer_name = "warren-dev"
           name = "vm-ukcg-dev-warren"  
           resource_group_name = "rg-ukcg-dev-warren"
         }
