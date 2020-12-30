190e5a020aa3634d0c47b4b0856219297892a6a7
Simon Thomas <simon.thomas@civica.co.uk>
2020-12-30 14:19:05 +0000

    add progress and fix path

--- resources/Initialize-VirtualMachine.ps1
+++ resources/Initialize-VirtualMachine.ps1
@@ -4,39 +4,45 @@
 $dir = Split-Path $MyInvocation.MyCommand.Path
 Write-Host "Current directory is $dir"
 
-# Change language and keyboard layout
+Write-Output "Changing language and keyboard layout" 
 Set-WinUserLanguageList -LanguageList en-GB -Force
 
-# Mount data disks
+Write-Output "Mount data disks"
 . "./Mount-DataDisks.ps1"
 
-# Install Chocolatey package manager
+Write-Output  "Install Chocolatey package manager"
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Since we can't restart terminal as such, the amendment to the path variable is not respected - hardcode path to exe
 $choco = "C:\ProgramData\chocolatey\bin\choco.exe"
-& $choco
-& $choco install git -y --no-progress
-Write-Output "Installed git?"
-git --version
 
-# Clone script repo to get remaining scripts and other resources
+Write-Output "PROGRESS: Test choco"
+& choco
+
+Write-Output "PROGRESS: Install git"
+& $choco install git -y --no-progress
+
+Write-Output "PROGRESS: Creating Source directory"
 New-Item -Path "E:\" -Name "Source" -ItemType "directory"
 $source = "E:\Source"
 Set-Location $source
+
+Write-Output "PROGRESS: Cloning repo get remaining scripts and other resources"
 git clone $Env:REPO_URL "vm-setup"
 Set-Location "vm-setup"
 Set-Location $Env:SCRIPT_DIR
 
+Write-Output "PROGRESS: Setting folder icon"
 . "./Set-FolderIcon.ps1"
 New-Item -Path $source -Name "desktop.ini" -ItemType "file"
-Set-FolderIcon -Icon "${Env:REPO_URL}\vm-setup\${Env:SCRIPT_DIR}\matrix_code.ico" -Path $source
+Set-FolderIcon -Icon "${source}\vm-setup\${Env:SCRIPT_DIR}\matrix_code.ico" -Path $source
 
-# Add Source to quick access:
+Write-Output "PROGRESS: Adding Source to quick access"
 $qa = New-Object -ComObject shell.application
 $qa.NameSpace($source).Self.InvokeVerb("pintohome")
 
-# Remove Videos from quick access:
+Write-Output "PROGRESS: Removing Videos from quick access"
 ($qa.Namespace("shell:::{679F85CB-0220-4080-B29B-5540CC05AAB6}").Items() | Where-Object { $_.Path -EQ "C:${env:HOMEPATH}\Videos" }).InvokeVerb("unpinfromhome")
-Write-Output "Success!"
+
+Write-Output "This is what success looks like!"
 exit 0
