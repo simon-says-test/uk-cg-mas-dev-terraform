f7eda326d0d3ea097a8a558c418934414ac5404e
Simon Thomas <simon.thomas@civica.co.uk>
2020-12-31 16:30:19 +0000

    tweak

--- resources/Initialize-VirtualMachine.ps1
+++ resources/Initialize-VirtualMachine.ps1
@@ -21,7 +21,7 @@ New-Item -Path "E:\" -Name "Source" -ItemType "directory"
 $repo = "E:/Source/vm-setup"
 
 Write-Output "PROGRESS: Creating scheduled task to clone repo, get remaining scripts and continue setup upon login"
-$argument = "-ExecutionPolicy Unrestricted -NoExit -Command & {git clone $Env:REPO_URL $repo; git pull; ./${repo}/${Env:SCRIPT_DIR}/Initialize-VirtualMachine2.ps1}'"
+$argument = "-ExecutionPolicy Unrestricted -NoExit -Command & {git clone $Env:REPO_URL $repo; git pull; & ${repo}/${Env:SCRIPT_DIR}/Initialize-VirtualMachine2.ps1}"
 $action = New-ScheduledTaskAction -Execute "powershell" -Argument $argument
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 $principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
