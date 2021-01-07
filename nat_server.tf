ec2b177d0c53c254e0a1ad378109b5cb698768a4
Warren Jones <warren.jones@civica.co.uk>
2021-01-07 11:14:34 +0000

    Revert previous change and remove Public IP Prefix

--- nat_server.tf
+++ nat_server.tf
@@ -8,22 +8,12 @@ resource "azurerm_public_ip" "nat" {
   resource_group_name = azurerm_resource_group.nat.name
   location            = azurerm_resource_group.nat.location
   allocation_method   = "Static"
-  zones               = ["1"]
-}
-
-resource "azurerm_public_ip_prefix" "nat" {
-  name                = "pippr-nat-uksouth-001"
-  location            = azurerm_resource_group.nat.location
-  resource_group_name = azurerm_resource_group.nat.name
-  prefix_length       = 30
-  zones               = ["1"]
 }
 
 resource "azurerm_nat_gateway" "nat" {
   name                    = "nat-gateway-uksouth-001"
   location                = azurerm_resource_group.nat.location
   resource_group_name     = azurerm_resource_group.nat.name
-  public_ip_prefix_ids    = [azurerm_public_ip_prefix.nat.id]
   sku_name                = "Standard"
   idle_timeout_in_minutes = 10
   zones                   = ["1"]
