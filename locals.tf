77f541ae9f7b792c0de561d3c1bb881a64a27995
Simon Thomas <simon.thomas@civica.co.uk>
2021-01-15 16:16:43 +0000

    upper case resource groups to see if fix issues with replacement

--- locals.tf
+++ locals.tf
@@ -4,19 +4,19 @@ locals {
           computer_name = "simon"
           username = "7956891c-a114-4d6a-9565-ae33ad53aacf"
           name = "vm-ukcg-dev-simon"  
-          resource_group_name = "rg-ukcg-dev-simon-1"
+          resource_group_name = "RG-UKCG-DEV-SIMON"
         },
         { 
           computer_name = "warren"
           username = "e41bbfca-379a-4159-bd56-4906ce5bf237"
           name = "vm-ukcg-dev-warren"  
-          resource_group_name = "rg-ukcg-dev-warren-1"
+          resource_group_name = "RG-UKCG-DEV-WARREN"
         },
         { 
           computer_name = "matt"
           username = "06020d9c-647f-41d7-aa89-868fa651804a"
           name = "vm-ukcg-dev-matt"  
-          resource_group_name = "rg-ukcg-dev-matt-1"
+          resource_group_name = "RG-UKCG-DEV-MATT"
         }
     ]
 }
