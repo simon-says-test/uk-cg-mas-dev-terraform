cee5640635cc1c483ca577d8a5cd9c7fdbeff893
Simon Thomas <simon.thomas@civica.co.uk>
2021-01-15 12:27:08 +0000

    try with id instead

--- locals.tf
+++ locals.tf
@@ -2,19 +2,19 @@ locals {
     vm_params = [ 
         { 
           computer_name = "simon"
-          username = "simon.thomas@civica.co.uk"
+          username = "7956891c-a114-4d6a-9565-ae33ad53aacf"
           name = "vm-ukcg-dev-simon"  
           resource_group_name = "rg-ukcg-dev-simon"
         },
         { 
           computer_name = "warren-dev"
-          username = "warren.jones@civica.co.uk"
+          username = "e41bbfca-379a-4159-bd56-4906ce5bf237"
           name = "vm-ukcg-dev-warren"  
           resource_group_name = "rg-ukcg-dev-warren"
         },
         { 
           computer_name = "matt-dev"
-          username = "matthew.robinson@civica.co.uk"
+          username = "06020d9c-647f-41d7-aa89-868fa651804a"
           name = "vm-ukcg-dev-matt"  
           resource_group_name = "rg-ukcg-dev-matt"
         }
