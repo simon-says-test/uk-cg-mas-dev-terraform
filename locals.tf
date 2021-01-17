9fb7ed06601ce9fc00750bca9eec97f6e9cd42f9
Simon Thomas <simon.thomas@civica.co.uk>
2021-01-17 11:22:09 +0000

    Update locals.tf
    minor

--- locals.tf
+++ locals.tf
@@ -2,18 +2,13 @@ locals {
     vm_params = [ 
         { 
           unique_short_name = "simon"
-          userId = "7956891c-a114-4d6a-9565-ae33ad53aacf"
-          email = "simon.thomas@civica.co.uk"  
+          userId = "AD object ID"
+          email = "simon.surname@civica.co.uk"  
         },
         { 
           unique_short_name = "warren"
-          userId = "e41bbfca-379a-4159-bd56-4906ce5bf237"
-          email = "warren.jones@civica.co.uk"  
-        },
-        { 
-          unique_short_name = "matt"
-          userId = "06020d9c-647f-41d7-aa89-868fa651804a"
-          email = "matthew.robinson@civica.co.uk"  
+          userId = "AD object ID"
+          email = "warren.surname@civica.co.uk"  
         }
     ]
 }
